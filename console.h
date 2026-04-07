f17da7464b544d4a6bea27e21aba1434442da07b
polina375 <apollinariya375@gmail.com>
2026-04-07 13:58:18 +0300

    Add comments

--- console.h
+++ console.h
@@ -2,6 +2,8 @@
 #include <iostream>
 
 namespace Console {
+    // Выводит информационное сообщение с префиксом [INFO].
     static void info(const char* msg) { std::cout << "[INFO] " << msg << "\n"; }
+    // Выводит имя переменной и её значение типа float.
     static void value(const char* name, float val) { std::cout << name << ": " << val << "\n"; }
 }; 
