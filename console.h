7b9c66c11b476e554ee625998c0353fd4b437015
polina375 <apollinariya375@gmail.com>
2026-04-07 13:50:06 +0300

    Refactor Console class to Console namespace

--- console.h
+++ console.h
@@ -1,11 +1,7 @@
 #pragma once
 #include <iostream>
 
-class Console {
-public:
-    // Выводит информационное сообщение с префиксом [INFO].
+namespace Console {
     static void info(const char* msg) { std::cout << "[INFO] " << msg << "\n"; }
-
-   // Выводит имя переменной и её значение типа float.
     static void value(const char* name, float val) { std::cout << name << ": " << val << "\n"; }
-};
+}; 
